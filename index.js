8072c6ca414640209d686ac059f7acb9f99195cd
Amy Chan <amy.chansya@gmail.com>
2022-10-12 21:22:00 -0400

    Defining post route

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ app.get('/tacos', (req,res)=>{
     res.send('GET /tacos response')
 })
 
+app.post('/tacos', (req,res)=>{
+    res.send('POST /tacos response')
+})
+
 app.listen(3000, ()=>{
     console.log("On port 3000")
 })
