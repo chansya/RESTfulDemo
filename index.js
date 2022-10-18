0d5c079c5f270eb6dfb890afe5c9796a463a5778
Amy Chan <amy.chansya@gmail.com>
2022-10-18 17:00:52 -0400

    Restful new

--- index.js
+++ index.js
@@ -1,9 +1,46 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 
 // middleware to tell express to parse post request
 app.use(express.urlencoded({ extended : true }))
 app.use(express.json())
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine','ejs');
+
+const comments = [
+    {
+        username: 'Todd',
+        comment: 'hahaha'
+    },
+    {
+        username: 'Tedd',
+        comment: 'hehehe'
+    },
+    {
+        username: 'Tudd',
+        comment: 'huhuhu'
+    },
+    {
+        username: 'Tadd',
+        comment: 'hohoho'
+    },
+]
+
+app.get('/comments', (req,res)=>{
+    res.render('comments/index', {comments})
+})
+
+app.get('/comments/new', (req, res)=>{
+    res.render('comments/new')
+})
+
+app.post('/comments', (req,res)=>{
+    console.log(req.body)
+    const {username, comment} = req.body;
+    comments.push({username, comment})
+    res.send('It worked.')
+})
 
 app.get('/tacos', (req,res)=>{
     res.send('GET /tacos response')
